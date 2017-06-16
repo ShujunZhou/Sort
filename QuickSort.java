eddfa9fa4d87ae9b4991d52b796906100440f8e7
Shu <1129220400@qq.com>
2017-06-16 11:00:09 +0800

    快速排序

--- QuickSort.java
+++ QuickSort.java
@@ -35,7 +35,7 @@ public class QuickSort {
     }
 
     public static void main(String[] args) {
-        int[] nums = {-1, -2, 3, 4, 1, 0};
+        int[] nums = {-1, -2, 3, 4, 1, 5};
         quickSort(nums);
         for (int i = 0; i < nums.length; ++i) {
             System.out.println(nums[i]);
